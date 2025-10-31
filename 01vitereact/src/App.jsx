429209e0e614eef09fea683fb77b45dad0cd454e
akshaay-r <akshay.ramachandran.ra@gmail.com>
2025-10-31 12:53:53 +0530

    Custom JSx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,12 @@
 import Newfile from './newFile'
 
 function App() {
+   const username = "Akshay"
 
   return (
     <>
       <Newfile/>
-      <h1>NewFile is ready</h1>
+      <h1>NewFile is ready {username}</h1>
     </>
     
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+
+
+// const reactElement = {
+//     type : 'a',
+//     props : {
+//         href : 'https://gooogle.com',
+//         target : '_blank'
+//     },
+//     children : 'Cick me to visi google'
+// };
+ 
+const anotherUser = " Abhilash"
+
+const reactElement = React.createElement(
+    'a',
+    {
+      href : "http://google.com",
+      target : '_blank',
+    },
+    'click me to visit',
+    anotherUser
+)
+
+const anotheElement = (
+  <a href="htpp://google.com" target='_blank'> visit google</a>
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    reactElement
+    // <App/>
+  
 )
