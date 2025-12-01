8031ad65d88aec48719477c170aed710979244e5
akshaay-r <akshay.ramachandran.ra@gmail.com>
2025-12-01 14:14:48 +0530

    Custom compoonent input

--- 12megablog/src/components/index.js
+++ 12megablog/src/components/index.js
@@ -1,4 +1,7 @@
+import container from "./container/container";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Logo from "./Logo";
+import LogoutBtn from "./LogoutBtn";
 
-export { Header, Footer };
+export { Header, Footer, container, Logo, LogoutBtn };
